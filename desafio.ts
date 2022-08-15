ecf45d4fa10560f5d584b356145ad39a68e40846
Loureiroa <arthurldr199@gmail.com>
2022-08-14 23:47:43 -0300

    first commit

---
+++ desafio.ts
@@ -0,0 +1,58 @@
+// Como podemos melhorar o esse código usando TS? 
+
+//Desafio n2
+
+
+enum Profissao {
+    Atriz,
+    Padeiro,
+    atriz,
+    padeiro
+}
+
+interface Pessoa {
+
+    nome:string, 
+    idade: number;
+    profissao: Profissao
+
+}
+
+const pessoa1: Pessoa = {
+    nome: 'maria',
+    idade: 29,
+    profissao: Profissao.Atriz
+
+}
+
+const pessoa2: Pessoa = {
+    nome:"roberto",
+    idade: 19,
+    profissao: Profissao.padeiro
+    
+}
+
+const pessoa3: Pessoa = {
+    nome: "laura",
+    idade: 32,
+    profissao: Profissao.atriz
+}
+
+let pessoa4: Pessoa = {
+    nome: "carlos",
+    idade: 19,
+    profissao: Profissao.Padeiro
+}
+
+
+function listar(lista: string ) {
+
+    for( const item of lista) {
+
+        console.log('-', item);
+    }
+
+}
+
+
+listar (pessoa1.nome);
